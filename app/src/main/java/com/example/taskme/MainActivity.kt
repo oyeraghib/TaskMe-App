c69840aa27647a8ece163847e351e6f29ad003b4
oyeraghib <raghib.khan76@gmail.com>
2021-09-12 20:58:48 +0530

    update UI add dependencies

--- app/src/main/java/com/example/taskme/MainActivity.kt
+++ app/src/main/java/com/example/taskme/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.taskme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(Navigation.findNavController(this, R.id.nav_host_fragment), null)
     }
 }
